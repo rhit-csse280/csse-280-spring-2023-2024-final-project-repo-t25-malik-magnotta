f6eff76b2328f26ca83d98ae43d4ffe0f4346f6c
FellBunion <magnotcj@rose-hulman.edu>
2024-05-02 13:44:05 -0400

    edits to home page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,88 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
+var budget = budget || {};
 
-/** namespace. */
-var rhit = rhit || {};
+budget.UID = null;
 
-/** globals */
-rhit.variableName = "";
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+budget.FbAuthManager = class {
 	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
+	signIn() {
+		Rosefire.signIn("", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				if (error.code === 'auth/invalid-custom-token') {
+					console.log("The token you provided is not valid.");
+				} else {
+					console.log("signInWithCustomToken error", error.message);
+				}
+			});
+		});
+
 
 	}
-
-	methodName() {
-
+	signOut() {
+		firebase.auth().signOut();
+	}
+	get uid() {
+		return this._user.uid;
+	}
+	get isSignedIn() {
+		return !!this._user;
 	}
 }
 
-/* Main */
-/** function and class syntax examples */
+budget.LoginPageController = class {
+	constructor() {
+			var uiConfig = {
+				signInSuccessUrl: '/home.html',
+				signInOptions: [
+			  	firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			  	firebase.auth.EmailAuthProvider.PROVIDER_ID,
+				],
+		  	};
+		  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+		  ui.start('#firebaseui-auth-container', uiConfig);
+	}
+}
+
+
+
+initializePage = () => {
+	
+	if(document.querySelector("#loginPage")){
+
+	}
+	else if(document.querySelector("#homePage")){
+
+	}
+	else if(document.querySelector("#statsPage")){
+
+	}
+
+
+
+}
+
+
 rhit.main = function () {
 	console.log("Ready");
+
+	budget.LoginPageController();
+
+
+
 };
 
 rhit.main();
